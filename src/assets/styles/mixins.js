00c9bf716904b82b839a7616e4196e711236858b
UmarovIkrom <ikromme13@gmail.com>
2023-11-24 21:06:31 +0500

    unused files removed

--- src/assets/styles/mixins.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const font = (fontSize, lineHeight, fontWeight, color) => `
-  font-size: ${fontSize};
-  line-height: ${lineHeight};
-  font-weight: ${fontWeight};
-  color: ${color};
-`;
